86254a638f9fa81356fe0f049d149ba3145f8651
yoshdroid <yoshdroid@gmail.com>
2015-04-14 07:33:33 +0900

    add alcana-rules

---
+++ alcana-rules.rb
@@ -0,0 +1,28 @@
+# coding: utf-8
+# 2015.4.7
+
+#
+# AlcanaRules: 定数その他管理
+#
+module AlcanaRules
+  DeckLimit = 40
+  TurnLimit = 10
+  CostLimit = 12
+  LifeLimit = 7
+  
+  HandLimit = 7
+  UnitLimit = 5
+  TrigLimit = 4
+  
+  CostTable = [ [2,3,4,5,6,7,7,7,7,7], [3,3,4,5,6,7,7,7,7,7] ]
+  DrawTable = [ [0,2,2,2,2,2,2,2,2,2], [2,2,2,2,2,2,2,2,2,2] ]
+  
+  JokerGageLimit = 100
+  JokerGageDmgUp = 15
+  JokerSpeedRate = 5
+  
+  UnitLevelLimit = 3
+  
+  MulliganLimit = 7
+  FirstHand     = 4
+end # AlcanaRules
